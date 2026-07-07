3bc48dbca6805b5f473b31e64e5c68df636f67a1
shazzuxofficial-creator <shazzux.official@gmail.com>
2026-07-07 14:30:10 +0530

    Add files via upload

---
+++ custom_web/anon_msg.js
@@ -0,0 +1,34 @@
+// anon_msg.js
+
+const chatSequence = [
+    {
+        anonMsg: "Hey Rajesh, You are from india right.!?",
+        opt1: "How you know my name?",
+        opt2: "The hell.! Who are you?"
+    },
+    {
+        anonMsg: "I got a cool way to generate black money online and safe.",
+        opt1: "I am here for ..... , umm I guess for just educational purpose.!",
+        opt2: "I am just exploring... I Don't wanna do any illegal stuff"
+    },
+    {
+        anonMsg: "But you are not gonna find any educational content here. This is a black chat section, not a library.",
+        opt1: "Oh, I thought this is a place for learning.",
+        opt2: "ohkay .... , Am I gonna get in trouble for that? I just wanna learn about the dark web and how it works. I am not gonna do any illegal stuff. I just wanna know how to protect myself from hackers and scammers."
+    },
+    {
+        anonMsg: "The 'wiki-reading' and 'exploring' is just a polite lie. Almost everyone come for the education, but they stay for the illegal shit. In this network, 'authentic' is the rarest thing you'll find.",
+        opt1: "Am I completely anonymous here?",
+        opt2: "ummm... Okay lemme see how black money is generated here."
+    },
+    {
+        anonMsg: "Cool , Don't worry this is safe",
+        opt1: "What I have to do for this.?",
+        opt2: "Okay tell me the steps how to do it."
+    },
+    {
+        anonMsg: "Open this link and follow the instructions: https://tx88woo****43ytree.onion",
+        opt1: "...",
+        opt2: "..."
+    }
+];
